907e59ad59e88fa72a6c74a03d786cdaf8b57fbb
Ro <rkcheruk@ucsc.edu>
2024-08-20 21:24:28 -0700

    finished power of 2

---
+++ isPowerOfTwo.py
@@ -0,0 +1,21 @@
+# 231. Power of Two
+'''
+Given an int n, return TRUE if it si a poer of 2; else return FALSE
+n is a power of two, if x exists such that n == 2^x
+'''
+
+
+def isPowerOfTwo(n: int) -> bool:
+
+    return False
+
+
+def main():
+    n = 6
+    flag = isPowerOfTwo(n)
+    print(flag)
+
+
+
+if __name__ == '__main__':
+    main()
